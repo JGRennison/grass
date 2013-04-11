81fe48dc8af565185d7c4073d502614038a2b417
Jonathan G Rennison <j.g.rennison@gmail.com>
2013-04-11 01:46:10 +0100

    Add a generic test utilities include. Implement an inline null pointer check.

---
+++ include/test/testutil.h
@@ -0,0 +1,38 @@
+//  grass - Generic Rail And Signalling Simulator
+//
+//  WEBSITE: http://grss.sourceforge.net
+//
+//  NOTE: This software is licensed under the GPL. See: COPYING-GPL.txt
+//
+//  This program  is distributed in the  hope that it will  be useful, but
+//  WITHOUT   ANY  WARRANTY;   without  even   the  implied   warranty  of
+//  MERCHANTABILITY  or FITNESS  FOR A  PARTICULAR PURPOSE.   See  the GNU
+//  General Public License for more details.
+//
+//  Jonathan Rennison (or anybody else) is in no way responsible, or liable
+//  for this program or its use in relation to users, 3rd parties or to any
+//  persons in any way whatsoever.
+//
+//  You  should have  received a  copy of  the GNU  General Public
+//  License along  with this program; if  not, write to  the Free Software
+//  Foundation, Inc.,  59 Temple Place,  Suite 330, Boston,  MA 02111-1307
+//  USA
+//
+//  2013 - Jonathan Rennison <j.g.rennison@gmail.com>
+//==========================================================================
+
+#ifndef INC_TESTUTIL_ALREADY
+#define INC_TESTUTIL_ALREADY
+
+#include "catch.hpp"
+
+template <typename C> C* CheckPtr(C* in, const char *file, unsigned int line) {
+	if(!in) {
+		FAIL("CheckPtr failed. File: " << file << ", line: " << line);
+	}
+	return in;
+}
+
+#define PTR_CHECK(p) CheckPtr(p, __FILE__, __LINE__)
+
+#endif
